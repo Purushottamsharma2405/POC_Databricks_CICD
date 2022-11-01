fea170d13175ad4948f7f0bfe2b8668f42428ff5
Purushottamsharma2405 <80710209+Purushottamsharma2405@users.noreply.github.com>
2022-11-01 13:17:38 +0530

    First Commit

---
+++ cicd.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#Update System Local Repositories
+sudp apt-get update -y
+sudo apt-get upgrade -y
+
+# install python3 and pip 
+apt install python3 -y
+sudo apt install python3-pip -y
+
+#Installing required dependencies
+pip3 install -r requirement.txt
+
+export TOKEN="dapi8e16fd65fb3b46a4a37595587797f150"
+
+#Running out automaiton script
+python3 automationscript.py
